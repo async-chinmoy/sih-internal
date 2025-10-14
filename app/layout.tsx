ce18b369f1879d4453d2011873c7b36138dff4b8
Chinmoy Senapoti <chinmoysenapoti77x@gmail.com>
2025-10-14 09:28:06 +0530

    issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type='image/x-icon' href="/favicon.png" />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
       <ClientOnlyI18nProvider>
           {children}
